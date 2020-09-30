e9324fcf43cacf683eea63ce66ede47dc551f2af
maxxxLmixxx-rs <max.280599@gmail.com>
2020-09-30 19:58:21 +0300

    feat: /clever/g solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let matcher = bracketsConfig.map(([o, c]) => (
+    /[a-zA-Z0-9]/.test(o + c) ? o + c : '\\' + o + '\\' + c 
+  )).join('|'), regExp = new RegExp(matcher, "g");
+  while (regExp.test(str)) {
+    str = str.replace(regExp, '');
+  } return str.length === 0 ? true : false;
 }
